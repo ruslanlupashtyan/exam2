253e5ff9849a9101d40c15d163667685f9fb15e8
Пользователь <ruslanl1488@gmail.com>
2020-08-30 15:21:47 +0300

    111

--- js/script.js
+++ js/script.js
@@ -8,11 +8,26 @@ $('.slider_box').slick({
 });
 
 $('.news_slider').slick({
-    arrows: true,
+    // prevArrow: '<button type="button" class="slick_arrow slick-prev"><i class="fas fa-angle-left"></i></button>',
+    // nextArrow: '<button type="button" class="slick_arrow slick-next"><i class="fas fa-angle-right"></i></button>',
     dots: true,
-    dotsClass: "slider_dots",
-    infinite: true,
+    infinite: false,
+    speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
-    slide: ".news_slide"
+    autoplay: true,
+    arrows: false,
+    dotsClass: "slider_dots",
+    responsive: [
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: false,
+                dots: false
+            }
+        },
+    ]
 });
+
