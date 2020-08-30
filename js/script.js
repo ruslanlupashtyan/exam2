0b2b0d5617f1d3d6d668cc4e8878082de8dd396d
Пользователь <ruslanl1488@gmail.com>
2020-08-30 17:00:09 +0300

    222

--- js/script.js
+++ js/script.js
@@ -8,26 +8,38 @@ $('.slider_box').slick({
 });
 
 $('.news_slider').slick({
-    // prevArrow: '<button type="button" class="slick_arrow slick-prev"><i class="fas fa-angle-left"></i></button>',
-    // nextArrow: '<button type="button" class="slick_arrow slick-next"><i class="fas fa-angle-right"></i></button>',
+    prevArrow: '<button type="button" class="slick_arrow slick-prev"><i class="fas fa-angle-left"></i></button>',
+    nextArrow: '<button type="button" class="slick_arrow slick-next"><i class="fas fa-angle-right"></i></button>',
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
-    autoplay: true,
-    arrows: false,
+    // autoplay: true,
+    arrows: true,
     dotsClass: "slider_dots",
+    // mobileFirst: true,
     responsive: [
         {
             breakpoint: 767,
             settings: {
                 slidesToShow: 2,
                 slidesToScroll: 2,
-                infinite: false,
-                dots: false
+                infinite: true,
+                dots: false,
+                arrows: false,
             }
         },
+        {
+            breakpoint: 500,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false,
+                arrows: false
+            }
+        }
     ]
 });
 
